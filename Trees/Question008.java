59985ca8b5cef0c04c9d306671a9bda69cb2bd20
Rohan-src <rohanjagtap0852@gmail.com>
2026-01-09 22:21:47 +0530

    Print all elements at Nth level in Binary tree

---
+++ Trees/Question008.java
@@ -0,0 +1,66 @@
+package Trees;
+import java.util.*;
+public class Question008 {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static Node root=null;
+    public static void insert(int data){
+        Node node=new Node(data);
+        if(root==null){
+            root=node;
+            return;
+        }
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            Node current=queue.poll();
+            if(current.left==null){
+                current.left=node;
+                return;
+            }else{
+                queue.add(current.left);
+            }
+            if(current.right==null){
+                current.right=node;
+                return;
+            }else{
+                queue.add(current.right);
+            }
+        }
+    }
+    public static void levelprint(Node root,int n){
+        if(root==null) {
+            System.out.print("The tree is empty");
+            return;
+        }
+        if(n==1){
+            System.out.print(root.data+" ");
+            return;
+        }else {
+            levelprint(root.left, n - 1);
+            levelprint(root.right, n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter number of nodes:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<n;i++){
+            int data=sc.nextInt();
+            insert(data);
+        }
+        System.out.print("Enter level to print:- ");
+        int size=sc.nextInt();
+        levelprint(root,size);
+    }
+}
