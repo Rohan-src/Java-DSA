44da18acf1bc01fb9bf0d5164ec04a0686602578
Rohan-src <rohanjagtap0852@gmail.com>
2026-04-04 16:57:06 +0530

    Bubble Sort

---
+++ Sorting_Algo/Bubble_sort.java
@@ -0,0 +1,29 @@
+package Sorting_Algo;
+
+import java.util.*;
+
+public class Bubble_sort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
