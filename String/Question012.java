dcaac9ccdeec435839bee740103a4dec3a5e6e15
Rohan-src <rohanjagtap0852@gmail.com>
2026-05-09 08:25:00 +0530

    Remove All Adjacent Duplicates In String

---
+++ String/Question012.java
@@ -0,0 +1,21 @@
+package String;
+
+class Question012 {
+    public String removeDuplicates(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()) {
+
+            int len = sb.length();
+
+            if(len > 0 && sb.charAt(len - 1) == ch) {
+                sb.deleteCharAt(len - 1); // remove duplicate
+            } else {
+                sb.append(ch); // add character
+            }
+        }
+
+        return sb.toString();
+    }
+}
