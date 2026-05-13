0d878d0ef6783c33420848833a15011029239c73
Rohan-src <rohanjagtap0852@gmail.com>
2026-05-13 22:39:01 +0530

    print n factorial

---
+++ Recursion/Question02.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.*;
+
+public class Question02 {
+    public static int factorial(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+    public static void main(String[] args){
+        System.out.print("Enter n:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ans=factorial(n);
+        System.out.print(ans);
+    }
+}
